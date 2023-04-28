3f95009e9bc75859d27e239de87150026819aa73
Rodric <72948934+RodricBr@users.noreply.github.com>
2023-04-28 17:50:57 -0300

    Update netlas_domains_and_ip_recon.sh

--- netlas_domains_and_ip_recon.sh
+++ netlas_domains_and_ip_recon.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
+# Original variables
 IP_RESULTS="ips_from_netlas.txt"
 DOMAIN_RESULTS="domains_from_netlas.txt"
 ip_or_cidr_regex="^[0-9]+.[0-9]+.[0-9]+.[0-9]+(/[0-9]+)?$"
 domain_regex="^([a-zA-Z0-9-]*.)+([a-zA-Z]{2,}|xn--[[:alnum:]][a-zA-Z0-9-]*[[:alnum:]])$"
 
-if [ -z $1 ]; then
-   echo "USAGE: Create a file(s) with target domains and IPs. Then run this script with this file(s) as an argument(s)."
-   exit
-fi 
+# Colors Variables
+C_ERR="\x1b[31m" # Error
+C_INF="\x1b[96m" # Info
+C_END="\x1b[0m"  # Reset
 
-if [ -f $IP_RESULTS ]; then
-    echo "Error: File "$IP_RESULTS" is already exists in "$(pwd) >&2
-    exit -1
+[[ -z "$1" ]]&& echo -e "Usage:\n Create a file(s) with target domains and IPs.\n Then run this script with this file(s) as an argument(s)."
+
+# Checking if files exists
+if [ -f "$IP_RESULTS" ] && [ -f "$DOMAIN_RESULTS" ]; then
+  echo -e "${C_ERR}Error${C_END}: File \"$IP_RESULTS\" already exists in: $(pwd)" >&2
+  echo -e "${C_ERR}Error${C_END}: File \"$DOMAIN_RESULTS\" already exists in: $(pwd)" >&2
+  exit 2
 fi
 
-if [ -f $DOMAIN_RESULTS ]; then
-    echo "Error: File "$DOMAIN_RESULTS" is already exists in "$(pwd) >&2
-    exit -1
-fi
+# Cheating files
+> "$IP_RESULTS"
+> "$DOMAIN_RESULTS"
 
-touch $IP_RESULTS
-touch $DOMAIN_RESULTS
-
-for input_file in "$@"
+for input_file in "$@"; do
+#for target_ in $(cat "$input_file"); do # Original Loop using cat
+  while IFS= read -r target_; do
+    if [[ $target_ =~ $ip_or_cidr_regex ]]; then # IP or CIDR Section
+      echo "$target_"
+      temp_ips="$(echo "$(<$IP_RESULTS)")"; temp_domains="$(echo "$(<$DOMAIN_RESULTS)")"
+      search_="$(netlas download -d domain -c 10000 -i domain,a "domain:* a:\"$target_\"")"
+      new_ips="$(printf "%s\n" "$search_" | jq .data.a | tr -d "\" [],")"
+      new_domains="$(printf "%s\n" "$search_" | jq -r .data.domain)"
+      sort -uV "$temp_ips" "$new_ips" | grep -v "^$" > "$IP_RESULTS"
+      sort -u "$temp_domains" "$new_domains" | grep -v "^$" > "$DOMAIN_RESULTS"
+    elif [[ $target_ =~ $domain_regex ]]; then # Domain Section
+      echo "$target_"
+      temp_ips="$(echo "$(<$IP_RESULTS)")"; temp_domains="$(echo "$(<$DOMAIN_RESULTS)")"
+      search_="$(netlas download -d domain -c 10000 -i domain,a "domain:/(.*\.)?$target_/ a:*")"
+      new_ips="$(printf "%s\n" "$search_" | jq .data.a | tr -d "\" [],")"
+      new_domains="$(printf "%s\n" "$search_" | jq -r .data.domain)"
+      sort -uV "$temp_ips" "$new_ips" | grep -v "^$" > "$IP_RESULTS"
+      sort -u "$temp_domains" "$new_domains" | grep -v "^$" > "$DOMAIN_RESULTS"
+    else # Unrecognized target
+      echo -e "${C_ERR}Error${C_END}: Unrecognized target \"$target_\" in file \"$input_file\"." >&2
+    fi
+  done < <(echo "$(<$input_file)") # File that "while" will read
 do
-	for target in $(cat $input_file) 
-	do
-        if [[ $target =~ $ip_or_cidr_regex ]]; then   #IP or CIDR section
-            echo $target
-            temp_ips=$(cat $IP_RESULTS); temp_domains=$(cat $DOMAIN_RESULTS)
-            search=$(netlas download -d domain -c 10000 -i domain,a "domain:* a:\"$target\"")
-            new_ips=$(printf "%s\n" $search | jq .data.a | tr -d "\" [],")
-            new_domains=$(printf "%s\n" $search | jq -r .data.domain)
-            echo "$temp_ips" "$new_ips" | sort -uV | grep -v "^$"> $IP_RESULTS
-            echo "$temp_domains" "$new_domains" | sort -u | grep -v "^$"> $DOMAIN_RESULTS
-        elif [[ $target =~ $domain_regex ]]; then   #Domain section
-            echo $target
-            temp_ips=$(cat $IP_RESULTS); temp_domains=$(cat $DOMAIN_RESULTS)
-            search=$(netlas download -d domain -c 10000 -i domain,a "domain:/(.*\.)?$target/ a:*")
-            new_ips=$(printf "%s\n" $search | jq .data.a | tr -d "\" [],")
-            new_domains=$(printf "%s\n" $search | jq -r .data.domain)
-            echo "$temp_ips" "$new_ips" | sort -uV | grep -v "^$"> $IP_RESULTS
-            echo "$temp_domains" "$new_domains" | sort -u | grep -v "^$"> $DOMAIN_RESULTS
-        else    #Unrecognized target
-            echo 'Error: Unrecognized target '$target' in file '$input_file >&2
-        fi
-    done
-done
 
-echo "Found "$(cat $IP_RESULTS | wc -l)" IP addresses and "$(cat $DOMAIN_RESULTS | wc -l)" domains"
+echo -e "${C_INF}Found${C_END} \"$(wc -l $IP_RESULTS)\" ${C_INF}IP addresses and${C_END} \"$(wc -l $DOMAIN_RESULTS)\" ${C_INF}domains.${C_END}\n"
 #rm $IP_RESULTS $DOMAIN_RESULTS
+
+# Unsetting the variables assigned in the script
+unset temp_ips temp_domains search_ new_ips new_domains IP_RESULTS DOMAIN_RESULTS ip_or_cidr_regex domain_regex C_ERR C_INF C_END
